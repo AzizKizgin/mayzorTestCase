2052e80fee0a444802b465a3fb3fd363f43e03bd
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-04 01:41:08 +0300

    feat: toast message added

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import {Box, NativeBaseProvider, Text} from 'native-base';
 import appTheme from './src/theme/appTheme';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigation from './src/navigation/AppNavigation';
+import {ToastMessageProvider} from 'rn-toast-message';
 
 const App = () => {
   return (
     <NativeBaseProvider theme={appTheme}>
-      <NavigationContainer>
-        <AppNavigation />
-      </NavigationContainer>
+      <ToastMessageProvider>
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </ToastMessageProvider>
     </NativeBaseProvider>
   );
 };
