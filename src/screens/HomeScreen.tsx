9e61a4eae8962bb2907cba8b38bd65fc2eb37b5f
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-05 02:45:36 +0300

    feat: navigation to detail added and code refactored

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import {Box, FlatList} from 'native-base';
+import {Box, FlatList, Pressable} from 'native-base';
 import Header from '../components/molecules/Header';
 import TodaysDiscountsInfo from '../components/molecules/TodaysDiscountsInfo';
 import ProductItem from '../components/organisms/ProductItem';
 import ListFooter from '../components/organisms/ListFooter';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+const data: {
+  products: Product[];
+} = require('../data/productData.json');
 
 const HomeScreen = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<HomeNavigationParams>>();
+
+  const renderItem = (item: Product) => {
+    return (
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Details', {id: item.id});
+        }}>
+        <ProductItem key={item.id.toString()} product={item} />
+      </Pressable>
+    );
+  };
   return (
     <Box flex={1} backgroundColor={'background'}>
       <Header />
       <FlatList
         padding={'l'}
-        data={[1, 2, 3, 4, 5]}
-        renderItem={({index}) => <ProductItem key={index.toString()} />}
+        data={data.products}
+        renderItem={({item}) => renderItem(item)}
         ListHeaderComponent={<TodaysDiscountsInfo />}
         ListFooterComponent={<ListFooter key={'listFooter'} />}
       />
