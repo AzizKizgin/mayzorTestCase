41ce8d117a6876c914296a72be380bc0587cebc3
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-05 19:05:24 +0300

    feat: screen updated

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import {Box, Text} from 'native-base';
+import {Box, ScrollView, VStack} from 'native-base';
+import DetailImage from '../components/organisms/DetailImage';
+import {ProductProvider} from '../context/ProductContext';
+import DetailProductOptions from '../components/organisms/DetailProductOptions';
+
+const data: {
+  products: Product[];
+} = require('../data/productData.json');
 
 const DetailScreen = () => {
   return (
-    <Box>
-      <Text>DetailScreen</Text>
-    </Box>
+    <ProductProvider product={data.products[0]}>
+      <Box flex={1} backgroundColor={'background'}>
+        <ScrollView>
+          <DetailImage />
+          <Box flex={1} paddingX={'l'}>
+            <DetailProductOptions />
+          </Box>
+        </ScrollView>
+      </Box>
+    </ProductProvider>
   );
 };
 
